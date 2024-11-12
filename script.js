e297e523711419bd11ae1a65b3caf0e5873f50da
oman <smithowain@hotmail.com>
2024-11-12 10:37:56 +0900

    Array updating. Fixing errors with board

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// an array for the gameboard
+// an array for the gameboard. Build a board.
 
 const game = (function () {
   const gameArrayVar = ["","","","","","","","",""];
@@ -6,17 +6,23 @@ const game = (function () {
   const ticGameboard=document.getElementById("gameboard");
 
   const renderBoard = (function () {
-    for (let i=0;i<9;i++) {
-      if(ticGameboard.hasChildNodes===true){
-        ticGameboard.removeChild("box"+i)
-      }
+    for (let i=0;i<9;i++) {     
     const newDiv=document.createElement("div");
-    newDiv.id="box"+i
-    newDiv.classList="boardBoxes"
+    newDiv.id=i
+    newDiv.classList="boardBoxes";
     newDiv.innerText=gameArrayVar[i];
     ticGameboard.appendChild(newDiv);
     }
-    })
+  })
+
+  const updateBoard = (arrayIndex, number) => {
+    for (let i=0;i<9;i++) {
+      const boxToRemove = document.getElementById(i);
+      const garbage = ticGameboard.removeChild(boxToRemove);
+    };
+    gameArrayVar[arrayIndex] = number;
+    game.renderBoard();
+  }
 
   const getBoard = () => gameArrayVar;
 
@@ -25,6 +31,7 @@ const game = (function () {
 
   })
   return {
+    updateBoard,
     getBoard,
   renderBoard,
   ResetBoard}
@@ -32,24 +39,57 @@ const game = (function () {
 
 game.renderBoard();
 
+// begin game with button
+
+
+
+// activate player turns
+
+// Allow clicks to board
+
 const activateBoard = (function () {
-  boxes = document.querySelectorAll(".boardBoxes");
+
+  let whoseTurn=1;
+
+  const boxes = document.querySelectorAll(".boardBoxes");
 
   function clicked (e) {
-    console.log(e);
-  const clickedBox = document.getElementById(e.target.id);
-  clickedBox.innerText="X";
+  const boxNumber = parseInt(e.target.id);
+  if(whoseTurn===1) {
+    game.updateBoard(boxNumber,"X");
+  }else{
+    game.updateBoard(boxNumber,"O");    
+  }
+  game.renderBoard;
+  switchPlayer();
   }
 
-  boxes.forEach((box) => {box.addEventListener("click", clicked);
+  function switchPlayer () {
+    console.log(game.getBoard())
+    if (whoseTurn===1) {
+      whoseTurn=2;
+    }else {
+      whoseTurn=1;
+    }};
 
-  });
+  boxes.forEach((box) => {box.addEventListener("click", clicked);})
 
-})();
-
-
+  })();
 
 
+
+  const beginGame = (function () {
+    const startButton = document.getElementById("startBut");
+    let beginGameVar=false;
+    startButton.addEventListener("click", beginNewGame)
+  
+    function beginNewGame () {
+        if (beginGameVar===false) {
+        beginGameVar=true;
+        console.log("hello")
+        activateBoard.switchPlayer;
+      }
+    }})();
 
 
 // a function to build the board grid
