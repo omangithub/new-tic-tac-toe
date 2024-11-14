7672b036a7a9bdb1924800a16056121c40cff3f5
oman <smithowain@hotmail.com>
2024-11-14 14:23:22 +0900

    Game functions correctly. Just need to make player names alterable

--- script.js
+++ script.js
@@ -23,12 +23,19 @@ const game = (function () {
   };
   }
 
-  const getBoard = () => gameArrayVar;
+  const getBoard = (function() {
+    return gameArrayVar;
+  })
 
   const resetBoard = (function () {
     gameArrayVar = ["","","","","","","","",""];
+    for (let i=0;i<9;i++) {
+      game.updateBoard(i,"");
 
+    } 
+    playerTurn.resetTurn();
   })
+
   return {
     updateBoard,
     getBoard,
@@ -48,28 +55,39 @@ const playerTurn = (function () {
   let board=game.getBoard();
 
   const updateTurn = (boxNumber) => {
+    board=game.getBoard();
     if(whoseTurn===1 && board[boxNumber]==="") {
+
       game.updateBoard(boxNumber,"X");
       announceText.innerText="Player Two's turn"
       testWinner();
+      if(whoseTurn!==3){
       whoseTurn=2;
-
-      console.log(whoseTurn);
+      }
     }else if(whoseTurn===2 && board[boxNumber]==="") {
       game.updateBoard(boxNumber,"O"); 
       announceText.innerText="Player One's turn"
       testWinner();
+      if(whoseTurn!==3){
       whoseTurn=1;
-
-  }}
-
-  const resetTurn = ()=>{
-    whoseTurn=1
+      }
+    }else if(whoseTurn===3){
+      announceText.innerText="Reset the board to begin a new game." 
+    }
   }
+
+  const winnerAnounced = (function () {
+    whoseTurn=3
+  })
+
+  const resetTurn = (function () {
+    whoseTurn=1
+  })
 
   const getTurn = ()=> whoseTurn;
 
   return {
+    winnerAnounced,
     updateTurn,
     getTurn,
     resetTurn
@@ -81,7 +99,6 @@ const playerTurn = (function () {
 
 const activateBoard = (function () {
 
-  let turn=playerTurn.getTurn();
   let boxNumber = 0;
 
   function clicked (e) {
@@ -92,29 +109,34 @@ const activateBoard = (function () {
   const makeMove = (function (boxNumber) {
 
   playerTurn.updateTurn(boxNumber);
-  game.updateBoard();
   });
+
+  const resetboxNumber = (function() {
+    boxnumber = 0;
+  })
 
   return {
     makeMove,
-    clicked
+    clicked,
   }})();
 
   // begin game with button
 
 const beginGame = (function () {
     const startButton = document.getElementById("startBut");
-//    let beginGameVar=false;
+    let announceText = document.getElementById("announcer");
+    let board=game.getBoard();
+    console.log(board);
 
     const beginNewGame = (function () {
-//        if (beginGameVar===false) {
-//        beginGameVar=true;
-//        console.log("hello")
+
+      if (playerTurn.getTurn!==3 && !board.includes("X") && !board.includes("O")) {
           for (let i=0;i<9;i++) {
             let targetBox = document.getElementById(i);
             targetBox.addEventListener("click", activateBoard.clicked);
           }
-      });
+      announceText.innerText="Play begins with Player One"
+      }});
 
       startButton.addEventListener("click", beginNewGame)
 
@@ -127,27 +149,34 @@ const beginGame = (function () {
 
 const resetGameBut = (function () {
   const resetButton = document.getElementById("resetBut");
+  let announceText = document.getElementById("announcer");
 
 
   const resetGame = (function () {
-    game.resetBoard()
-    console.log (game.getBoard())
-    game.updateBoard();
-    playerTurn.resetTurn();
+    game.resetBoard();
+    announceText.innerText="The game is reset.\nPlay starts with player 1." 
     })
 
     resetButton.addEventListener("click", resetGame)
 
   })();
 
-const testWinner = (function () {
+function testWinner () {
   let announceText = document.getElementById("announcer");
   let boxes=game.getBoard();
+  let gameEnd=playerTurn.getTurn();
+
+  if (gameEnd!==3) {
   if (boxes[0]==="X" && boxes[1]==="X" && boxes[2]==="X" || boxes[3]==="X" && boxes[4]==="X" && boxes[5]==="X" || boxes[6]==="X" && boxes[7]==="X" && boxes[8]==="X" || boxes[0]==="O" && boxes[1]==="O" && boxes[2]==="O" || boxes[3]==="O" && boxes[4]==="O" && boxes[5]==="O" || boxes[6]==="O" && boxes[7]==="O" && boxes [8]==="O") {
-    announceText.innerText=`That's three in a row.\nPlayer ${playerTurn.getTurn()} is Champion!`
+  announceText.innerText=`That's three in a row.\nPlayer ${playerTurn.getTurn()} is Champion!`
+  playerTurn.winnerAnounced();
   } else if (boxes[0]==="X" && boxes[3]==="X" && boxes[6]==="X" || boxes[1]==="X" && boxes[4]==="X" && boxes[7]==="X" || boxes[2]==="X" && boxes[5]==="X" && boxes [8]==="X" || boxes[0]==="O" && boxes[3]==="O" && boxes[6]==="O" || boxes[1]==="O" && boxes[4]==="O" && boxes[7]==="O" || boxes[2]==="O" && boxes[5]==="O" && boxes [8]==="O") {
   announceText.innerText=`That's a column of three.\nPlayer ${playerTurn.getTurn()} is Champion!`
+  playerTurn.winnerAnounced();
   } else if (boxes[0]==="X" && boxes[4]==="X" && boxes[8]==="X" || boxes[2]==="X" && boxes[4]==="X" && boxes[6]==="X" || boxes[0]==="O" && boxes[4]==="O" && boxes[8]==="O" || boxes[2]==="O" && boxes[4]==="O" && boxes[6]==="O") {
   announceText.innerText=`That's three diagnoally.\nPlayer ${playerTurn.getTurn()} is Champion!`
-  }
-})
+  playerTurn.winnerAnounced();
+
+  }}
+
+};
